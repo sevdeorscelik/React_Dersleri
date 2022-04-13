4367ce58b592294619b24ab61478cf687ae364b9
Sevde <sevdeorscelik@gmail.com>
2022-04-13 15:03:16 +0200

    4. video

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,16 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-//functional component
+//functional component-----------------------------------------------------------------------------------
 
-function App() {
-    return <h1>Benim Adim Sevde</h1>;
+function App1() {
+    return <h1>Bu bir funktional component</h1>;
 }
 
 //componentimizi sayfada görebilmek icin ReactDOM'a ait render methodunu kullanmaliyiz
 
 
 ReactDOM.render( 
-    <App />,  //neyi göstermek istedigmizi yazariz
-    document.getElementById('root')  //nerede görmek istedigimizi yazariz
+    <App1 />,  //neyi göstermek istedigmizi yazariz
+    document.getElementById('root1')  //nerede görmek istedigimizi yazariz
+);
+
+//class component------------------------------------------------------------------------------------------
+
+class App2 extends React.Component {
+    render() {
+        return <h1>Bu bir class componenttir</h1>
+    }
+}
+
+ReactDOM.render( 
+    <App2 />,  //neyi göstermek istedigmizi yazariz
+    document.getElementById('root2')  //nerede görmek istedigimizi yazariz
 );
