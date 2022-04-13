e7e668ff74f6aa70be4271fe29c419bee5d263be
Sevde <sevdeorscelik@gmail.com>
2022-04-13 14:43:05 +0200

    first component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 
-const element = <h1>Sevde</h1>
+//functional component
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  element,
-  
+function App() {
+    return <h1>Benim Adim Sevde</h1>;
+}
+
+//componentimizi sayfada görebilmek icin ReactDOM'a ait render methodunu kullanmaliyiz
+
+
+ReactDOM.render( 
+    <App />,  //neyi göstermek istedigmizi yazariz
+    document.getElementById('root')  //nerede görmek istedigimizi yazariz
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
