ffd7d5e9d3558c684501d80f1cf7015f4d39bf6e
Sevde <sevdeorscelik@gmail.com>
2022-04-13 15:39:55 +0200

    jsx ile html arasindaki farklar

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -10,11 +10,13 @@ function App1() {
 
 //componentimizi sayfada görebilmek icin ReactDOM'a ait render methodunu kullanmaliyiz
 
-
 ReactDOM.render( 
     <App1 />,  //neyi göstermek istedigmizi yazariz
     document.getElementById('root1')  //nerede görmek istedigimizi yazariz
 );
+
+
+/********************************************************************
 
 //class component------------------------------------------------------------------------------------------
 
@@ -25,6 +27,49 @@ class App2 extends React.Component {
 }
 
 ReactDOM.render( 
-    <App2 />,  //neyi göstermek istedigmizi yazariz
-    document.getElementById('root2')  //nerede görmek istedigimizi yazariz
+    <App2 />,  
+    document.getElementById('root2')  
 );
+
+************************************************************************/
+
+
+//JSX  - syntax extension to js -------------------------------------------------------
+
+const names = ['Sevde', 'Semih', 'Misha']
+
+function App3() {
+    return ( // jsx yapisini düzenli olarak döndürebilmek icin hepsini tek ir parent elemente koyariz
+    <div> 
+        {names.map(name => (<h3> - {name} </h3>))}
+    </div>
+    
+    );
+}
+
+ReactDOM.render( 
+    <App3 />,  
+    document.getElementById('root3') 
+);
+
+
+// JSX ile style ----------------------------------------------------------------------
+//stil özelliklerini verirken html gibi atribut seklinde yazabilirz, ancak object seklinde yazariz
+//class ismi className seklinde verilir
+
+function App4() {
+
+    const buttonValue = 'My Jsx Button'
+    return ( 
+    <div> 
+        <button className='jsxClass' tabIndex="2"  type="button" style={{padding:'10px', backgroundColor:'pink', color:'gray', border: '2px solid brown'}}>{buttonValue}</button>
+    </div>
+    );
+}
+
+ReactDOM.render( 
+    <App4 />,  
+    document.getElementById('root4')  
+);
+
+
