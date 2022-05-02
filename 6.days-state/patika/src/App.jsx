01a182baf12d97f447bc92a06c309a54455e2d2e
Sevde <sevdeorscelik@gmail.com>
2022-05-02 04:58:09 +0200

    staTE

--- 6.days-state/patika/src/App.jsx
+++ 6.days-state/patika/src/App.jsx
@@ -3,21 +3,26 @@ import './App.css'
 
 function App() {
 
-  const[name, setName] = useState('Sevde'); //state#in varsayilan degeri sevde olur
-  const[age, setAge] = useState('27')
+  const [name, setName] = useState('Sevde'); //state#in varsayilan degeri sevde olur
+  const [age, setAge] = useState(27)
+  const [friends, setFriends] = useState(['zeynep', 'kübra'])
+  const [address, setAddress] = useState({city:'izmir', zip:12345})
 
   const changeName = () => {
     setName('Semih')
-    setAge('29')
+    setAge(29)
+    setFriends([...friends, 'seyma'])
+    setAddress({...address, title:'title'})
   }
 
- 
 
   return (
     <div className="App">
-     <h1>Merhabalar {name} ! </h1>
-     <h2> {age} </h2>
-     <button onClick={changeName} >Change</button>
+      <h1>Merhabalar {name} ! </h1>
+      <h2> {age} </h2>
+      {friends.map(friend => <p> {friend} </p>)}
+      <p>{address.city} + {address.zip}</p>
+      <button onClick={changeName} >Change</button>
     </div>
   )
 }
